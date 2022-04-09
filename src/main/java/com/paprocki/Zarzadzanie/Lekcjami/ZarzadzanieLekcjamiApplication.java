36e00d3613aa7103ec5a35b92bff915711598ad5
Tadeusz Paprocki <tadek.paprocki@gmail.com>
2022-04-09 11:10:10 +0200

    Initial Commit

---
+++ src/main/java/com/paprocki/Zarzadzanie/Lekcjami/ZarzadzanieLekcjamiApplication.java
@@ -0,0 +1,13 @@
+package com.paprocki.Zarzadzanie.Lekcjami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarzadzanieLekcjamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarzadzanieLekcjamiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paprocki/Zarzadzanie/Lekcjami/ZarzadzanieLekcjamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paprocki.Zarzadzanie.Lekcjami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarzadzanieLekcjamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
