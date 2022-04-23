1d53af945f9b484568ee00374b0aa108261c69fe
Tadeusz Paprocki <tadek.paprocki@gmail.com>
2022-04-23 11:57:15 +0200

    Added Student & StudentController

---
+++ src/main/java/com/paprocki/Zarzadzanie/Lekcjami/controller/StudentController.java
@@ -0,0 +1,60 @@
+package com.paprocki.Zarzadzanie.Lekcjami.controller;
+
+import com.paprocki.Zarzadzanie.Lekcjami.model.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        students.add(new Student("Mirosław Kowalski", "mirek@gmail.com", "Michał Leja", 200));
+        students.add(new Student("Jakub Nowicki", "jakub8899wp.pl", "Michał Leja", 200));
+    }
+
+    @GetMapping
+    public ResponseEntity getAllStudents() {
+        return new ResponseEntity(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity getSingleStudent(@PathVariable String email) {
+        return students.stream()
+                .filter(student -> student.getEmail().equals(email))
+                .findAny()
+                .map(student -> new ResponseEntity(student, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity("Brak studenta o danym emailu " + email, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity addStudent(@RequestBody Student student) {
+        if (students.stream().anyMatch(s -> s.getEmail().equals(student.getEmail()))) {
+            return new ResponseEntity("Konto z takim email już istnieje", HttpStatus.BAD_REQUEST);
+        }
+        students.add(student);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping()
+    public ResponseEntity editStudent(@RequestBody Student updatedStudent) {
+        if (students.stream().noneMatch(student -> student.getEmail().equals(updatedStudent.getEmail()))) {
+            return new ResponseEntity("Brak studenta o danym emailu", HttpStatus.BAD_REQUEST);
+        }
+        Student student = students.stream().filter(s -> s.getEmail().equals(updatedStudent.getEmail())).findAny().get();
+        student.setEmail(updatedStudent.getEmail());
+        student.setName(updatedStudent.getName());
+        student.setTeacher(updatedStudent.getTeacher());
+        student.setRate(updatedStudent.getRate());
+        return new ResponseEntity(student, HttpStatus.OK);
+    }
+
+}
